9798c90e907a34be0747e007f47f93f88339f6a2
MurphyL <im@murphyl.com>
2015-03-16 17:20:05 +0800

    数字格式

---
+++ src/test/java/api/test/number/NumberTest.java
@@ -0,0 +1,16 @@
+package api.test.number;
+
+import java.text.NumberFormat;
+
+import org.junit.Test;
+
+public class NumberTest {
+
+	@Test
+	public void testNumberFormat() {
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		System.out.println(nf.format(2.34569));
+		System.out.println(nf.format(11112.349));
+	}
+
+}
