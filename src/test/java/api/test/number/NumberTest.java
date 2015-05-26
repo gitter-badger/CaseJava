5333e9ba505a00e3ce68536460a30cbb8c8e30f7
MurphyL <im@murphyl.com>
2015-05-26 09:37:37 +0800

    小发现n

--- src/test/java/api/test/number/NumberTest.java
+++ src/test/java/api/test/number/NumberTest.java
@@ -2,15 +2,18 @@ package api.test.number;
 
 import java.text.NumberFormat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NumberTest {
+	
+	NumberFormat nf;
 
 	@Test
 	public void testNumberFormat() {
-		NumberFormat nf = NumberFormat.getNumberInstance();
-		System.out.println(nf.format(2.34569));
-		System.out.println(nf.format(11112.349));
+		nf = NumberFormat.getNumberInstance();
+		Assert.assertEquals("2.346", nf.format(2.34569));
+		Assert.assertEquals("11,112.349", nf.format(11112.349));
 	}
 
 }
