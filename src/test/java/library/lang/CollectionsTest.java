0ae2efd934135081d1979f3a4b26d98050801f05
MurphyL <im@murphyl.com>
2015-07-08 10:41:45 +0800

    集合测试

---
+++ src/test/java/library/lang/CollectionsTest.java
@@ -0,0 +1,40 @@
+package library.lang;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Predicate;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luohao4 on 2015/7/8.
+ */
+public class CollectionsTest {
+
+    @Test
+    public void testFilter() {
+        List<Integer> items = new ArrayList<Integer>(){{
+            this.add(0);
+            this.add(1);
+            this.add(2);
+            this.add(3);
+            this.add(4);
+            this.add(5);
+            this.add(6);
+            this.add(7);
+            this.add(8);
+            this.add(9);
+        }};
+        CollectionUtils.filter(items, new Predicate<Integer>() {
+            public boolean evaluate(Integer i) {
+                return i > 4;
+            }
+        });
+        for (Integer item : items) {
+            Assert.assertTrue("´óÓÚ 4", item > 4);
+        }
+    }
+
+}
