eb2b7020a2a4d7b9ce5f2f850077cbfee1bfb9f2
MurphyL <im@murphyl.com>
2015-07-14 11:35:22 +0800

    新增 testng 测试

---
+++ src/test/java/library/testng/HelloWorld.java
@@ -0,0 +1,15 @@
+package library.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by luohao4 on 2015/7/14.
+ */
+public class HelloWorld {
+
+    @Test
+    public void test(){
+        System.out.println(1);
+    }
+
+}
