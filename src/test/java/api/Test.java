709c45e1f966204ceff723335f2429f6ad97d39b
MurphyL <im@murphyl.com>
2016-01-07 17:49:42 +0800

    绕过泛型

---
+++ src/test/java/api/Test.java
@@ -0,0 +1,29 @@
+package api;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luohao4 on 2016/1/7.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("");
+        Class clazz = list.getClass();
+        try {
+            Method add = clazz.getDeclaredMethod("add", Object.class);
+            add.invoke(list, 1);
+            add.invoke(list, true);
+            System.out.println(list.get(1) instanceof String);
+            System.out.println(list.get(2) instanceof String);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
