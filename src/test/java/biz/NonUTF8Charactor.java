a9fce7d7a6b72572a4d2456cf053fdf1e57929cb
MurphyL <im@murphyl.com>
2016-02-03 17:12:20 +0800

    test reg

--- src/test/java/biz/NonUTF8Charactor.java
+++ src/test/java/biz/NonUTF8Charactor.java
@@ -2,6 +2,7 @@ package biz;
 
 import org.apache.commons.lang3.CharUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.regex.Pattern;
@@ -14,14 +15,12 @@ import static org.junit.Assert.assertTrue;
 public class NonUTF8Charactor {
 
     @Test
-    public void testFilterNonUTF8Charactor() {
-        String str = "若素 创意日式家用木餐具中式吃米饭碗喝汤碗圆套碗小菜碗套装\b大漆色大号\b龟甲碗";
-        System.out.println("若素 创意日式家用木餐具中式吃米饭碗喝汤碗圆套碗小菜碗套装\b大漆色大号\b龟甲碗");
-        Pattern reg = Pattern.compile("[\\x00-\\x08\\x1b\\x0b-\\x0c\\x0e-\\x1f\\x7f]");
-        System.out.println(reg.matcher(str).replaceAll(""));
-        String code = "号\b龟";
-        for (int i = 0; i < code.length(); i++) {
-            System.out.println(Integer.toHexString(code.charAt(i)));
+    public void testFilterNonUTF8Character() {
+        // Pattern reg = Pattern.compile("[\\x00-\\x08\\x1b\\x0b-\\x0c\\x0e-\\x1f\\x7f]");
+        Pattern reg = Pattern.compile("[\\S]");
+        String code = "\b";
+        if(reg.matcher(code).find()){
+            Assert.assertTrue(Integer.toHexString(code.charAt(0)).equals("8"));
         }
     }
     
