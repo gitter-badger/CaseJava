5230e916c12f43bbb22cc482b7eb6075da03d365
Luo Hao4(武汉_技术部_平台服务部_商管Luohao4逆向物流Luohao4网盟_罗昊) <luohao4@yhd.com>
2016-03-28 17:43:22 +0800

    joda-test

--- src/test/java/lib/joda/JodaTest.java
+++ src/test/java/lib/joda/JodaTest.java
@@ -5,14 +5,24 @@ import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.Weeks;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class JodaTest {
 
     Logger log = LogManager.getLogger(this.getClass());
 
     private DateTime dt;
+
+    private final String YYYY_MM_DD = "yyyy-MM-dd";
+    private final SimpleDateFormat F_YMD = new SimpleDateFormat(YYYY_MM_DD);
+    private final SimpleDateFormat F_E = new SimpleDateFormat("E");
+    private final SimpleDateFormat F_Y = new SimpleDateFormat("YYYY");
+    private final SimpleDateFormat F_H = new SimpleDateFormat("HH");
 
     private static final String DF = "yyyy-MM-dd HH:mm:ss SSS";
     private static final String DFB = "%04d-%02d-%02d %02d:%02d:%02d %03d"; // date formate blank
@@ -106,6 +116,63 @@ public class JodaTest {
 
         dt = dt.withTime(23, 59, 59, 999);
         log.debug("今天午夜：" + dt.toString(DF));
+    }
+
+    @Test
+    public void test() {
+        Date _now = new Date();
+        DateTime source = new DateTime(0);
+        DateTime now = new DateTime();
+        Assert.assertTrue(now.toDate().getTime() - _now.getTime() > 0);
+        Assert.assertEquals(source.toString(YYYY_MM_DD), "1970-01-01");
+        // 格式化时间
+        Assert.assertEquals(F_YMD.format(_now), now.toString(YYYY_MM_DD));
+        // 剖析 - 这里就直接使用过时的方法
+        Assert.assertEquals(WeekDay.toText(now.getDayOfWeek()) + "", F_E.format(_now));
+        Assert.assertEquals(now.getYearOfCentury(), Integer.valueOf(F_Y.format(_now)) % 100);
+        Assert.assertEquals(now.getHourOfDay() + "", F_H.format(_now));
+        // 计算 - 测试代码
+        for (int i = 0; i < 7; i++) {
+            Assert.assertEquals(WeekDay.toText(i + WeekDay.getOffset()), F_E.format(now.plusDays(i).toDate()));
+        }
+        DateTime feb30 = source.plusYears(now.getYear() - source.getYear()).plusMonths(1).plusDays(30);
+        System.out.println(feb30.toString(YYYY_MM_DD));
+        System.out.println(feb30.plusMonths(1).minusMonths(1).toString(YYYY_MM_DD));
+    }
+
+    enum WeekDay {
+        星期日(0),
+        星期一(1),
+        星期二(2),
+        星期三(3),
+        星期四(4),
+        星期五(5),
+        星期六(6);
+
+        private int code;
+
+        WeekDay(int code) {
+            this.code = code;
+        }
+
+        public static String toText(int code){
+            for (WeekDay weekDay : WeekDay.values()) {
+                if(weekDay.code == code % 7){
+                    return weekDay.name();
+                }
+            }
+            return "";
+        }
+
+        public static int getOffset(){
+            String name = new SimpleDateFormat("E").format(new Date());
+            for (WeekDay weekDay : WeekDay.values()) {
+                if(weekDay.name().equals(name)){
+                    return weekDay.code;
+                }
+            }
+            return -1;
+        }
     }
 
     // Apache Commons Lang 快速趋势分秒的方法
